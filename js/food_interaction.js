62b8d2563a55bb66197ef4c362c596c6ddc0513a
islamm771 <90573842+islamm771@users.noreply.github.com>
2024-02-09 22:16:30 +0200

    Add files via upload

---
+++ js/food_interaction.js
@@ -0,0 +1,21 @@
+let btn_back = document.querySelector('.back')
+let btn_show = document.querySelector(".toggle-nav");
+let btn_close = document.querySelector(".close");
+
+btn_back.onclick = function () { 
+    document.querySelector('aside').classList.toggle("open")
+    document.querySelectorAll('aside ul li a').forEach( c =>{
+        c.classList.toggle('open')
+    } )
+    // document.querySelector('.arrow').classList.toggle('d-none')
+    // document.querySelector('.bars').classList.toggle('d-none')
+}
+
+
+btn_show.onclick = function () {
+    document.querySelector("aside").classList.remove("close");
+ }
+btn_close.onclick = function () {
+  document.querySelector("aside").classList.add("close");
+};
+
